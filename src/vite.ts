cda645646163755c1afe85e09385c03bfc65e743
MellowCo <799478052@qq.com>
2022-08-26 20:39:29 +0800

    chore: ts lint

--- src/vite.ts
+++ src/vite.ts
@@ -2,4 +2,4 @@ import type { Options } from './types'
 import unplugin from '.'
 
 // TODO: some upstream lib failed generate invalid dts, remove the any in the future
-export default unplugin.vite as (options: Options) => any
+export default unplugin.vite as (options?: Options) => any
