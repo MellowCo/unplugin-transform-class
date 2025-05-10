d66c7d92a7596079880e03bf7e3e536a884db719
MellowCo <licl@>
2025-05-10 09:55:24 +0800

    chore: eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,15 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   type: 'lib',
-})
+},
+  {
+    ignores: [
+      '*.global.js',
+      'build',
+      'node_modules',
+      'interactive',
+      'examples',
+      'defaultConfig.ts',
+      '*.json',
+    ],
+  },)
